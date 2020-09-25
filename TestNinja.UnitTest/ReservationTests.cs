a16b7a7d9197d97a41cc8b2c354dd3391b9beb5f
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-09-26 02:24:59 +0300

    Add tests for Reservation.CanBeCanceledBy

--- TestNinja.UnitTest/ReservationTests.cs
+++ TestNinja.UnitTest/ReservationTests.cs
@@ -18,5 +18,34 @@ namespace TestNinja.UnitTest
             // Assert
             Assert.IsTrue(result);
         }
+
+        [TestMethod]
+        public void CanBeCanceledBy_OwnUser_ReturnsTrue()
+        {
+            // Arrange
+            var reservation = new Reservation();
+            var user = new User();
+            reservation.MadeBy = user;
+
+            // Act
+            var result = reservation.CanBeCancelledBy(user);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCanceledBy_AnyOtherUser_ReturnsFalse()
+        {
+            // Arrange
+            var reservation = new Reservation();
+            var user = new User();
+
+            // Act
+            var result = reservation.CanBeCancelledBy(user);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
     }
 }
