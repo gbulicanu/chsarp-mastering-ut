8ff1cca7ebdd6f8c4afebb207905b53cbcc9092e
Gheorghe Bulicanu <gicu@live.com>
2020-09-30 13:41:27 +0300

    Black-box test

--- TestNinja.UnitTest/MathTests.cs
+++ TestNinja.UnitTest/MathTests.cs
@@ -7,7 +7,7 @@ namespace TestNinja.UnitTest
     public class MathTests
     {
         [Test]
-        public void Add_WhenCalled_ShouldReturnTheSUmOfArguments()
+        public void Add_WhenCalled_ShouldReturnTheSumOfArguments()
         {
             // Arrange
             var math = new Math();
@@ -17,6 +17,45 @@ namespace TestNinja.UnitTest
 
             // Assert
             Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Max_FirstArgumentsIsGreater_ReturnTheFirstArgument()
+        {
+            // Arrange
+            var math = new Math();
+
+            // Act
+            var result = math.Max(2, 1);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_SecondArgumentsIsGreater_ReturnTheSecondArgument()
+        {
+            // Arrange
+            var math = new Math();
+
+            // Act
+            var result = math.Max(1, 2);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Max_ArgumentsAreEqual_ReturnTheSame()
+        {
+            // Arrange
+            var math = new Math();
+
+            // Act
+            var result = math.Max(1, 1);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(1));
         }
     }
 }
