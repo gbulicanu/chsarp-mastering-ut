e3ece4dbf17a88dc3c32c793b2be2bc22861fe08
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-10-02 13:07:00 +0300

    Testing methods that throw execeptions

---
+++ TestNinja.UnitTest/ErrorLoggerTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+
+using System;
+
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTest
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+        [Test]
+        public void Log_WhenCalled_SetLastErrorProperty() 
+        {
+            var errorLogger = new ErrorLogger();
+            const string errorMessage = "a";
+
+            errorLogger.Log(errorMessage);
+
+            Assert.That(errorLogger.LastError, Is.EqualTo(errorMessage));
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Log_InvalidError_ThrowsArgumentNullException(string error)
+        {
+            var errorLogger = new ErrorLogger();
+
+            Assert.That(
+                () => errorLogger.Log(error),
+                Throws.ArgumentNullException);
+        }
+    }
+}
