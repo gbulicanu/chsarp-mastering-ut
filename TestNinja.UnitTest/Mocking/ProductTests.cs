7a2f9e9f49e4e8b4489d1c157404772a53b3559d
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-10-06 12:45:16 +0300

    Example of mock abuse

---
+++ TestNinja.UnitTest/Mocking/ProductTests.cs
@@ -0,0 +1,35 @@
+﻿using Moq;
+
+using NUnit.Framework;
+
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTest.Mocking
+{
+    [TestFixture]
+    public class ProductTests
+    {
+        [Test]
+        public void GetPrice_GoldCustomer_Apply30PercentDiscount()
+        {
+            var product = new Product { ListPrice = 100 };
+            var customer = new Customer { IsGold = true };
+
+            var result = product.GetPrice(customer);
+
+            Assert.That(result, Is.EqualTo(70));
+        }
+
+        [Test]
+        public void GetPrice_GoldCustomer_Apply30PercentDiscount2()
+        {
+            var product = new Product { ListPrice = 100 };
+            var customer = new Mock<ICustomer>();
+            customer.Setup(x => x.IsGold).Returns(true);
+
+            var result = product.GetPrice(customer.Object);
+
+            Assert.That(result, Is.EqualTo(70));
+        }
+    }
+}
