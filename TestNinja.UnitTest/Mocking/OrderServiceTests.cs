601d30ee0d8f878b6cf1a7a1e1f4a1f8e26f5694
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-10-06 12:08:55 +0300

    Testing interaction between two methods (use only for external depndencies)

---
+++ TestNinja.UnitTest/Mocking/OrderServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using Moq;
+
+using NUnit.Framework;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTest.Mocking
+{
+    [TestFixture]
+    public class OrderServiceTests
+    {
+        [Test]
+        public void PaceOrder_WhenCalled_CallsStorage()
+        {
+            var storage = new Mock<IStorage>();
+            var orderService = new OrderService(storage.Object);
+            var order = new Order();
+
+            orderService.PlaceOrder(order);
+
+            storage.Verify(x => x.Store(order));
+        }
+    }
+}
