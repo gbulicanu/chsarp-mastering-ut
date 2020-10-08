d9ef8e7f2c820a0b0073c7da91f083a30a177c51
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-10-08 10:17:30 +0300

    Refactor EmployeeController for testability

---
+++ TestNinja/Mocking/EmployeeStorage.cs
@@ -0,0 +1,21 @@
+﻿using TestNinja.Mocking;
+
+namespace TestNinja.Mocking
+{
+    public class EmployeeStorage : IEmployeeStorage
+    {
+        private EmployeeContext _db;
+
+        public EmployeeStorage()
+        {
+            _db = new EmployeeContext();
+        }
+
+        public void DeleteEmployee(int id)
+        {
+            var employee = _db.Employees.Find(id);
+            _db.Employees.Remove(employee);
+            _db.SaveChanges();
+        }
+    }
+}
