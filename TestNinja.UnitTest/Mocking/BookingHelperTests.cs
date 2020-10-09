d37ae289a16449d6b6a041b06c7fc464abff14f3
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-10-09 16:09:38 +0300

    Add remaining unit tests for BookingHelper

--- TestNinja.UnitTest/Mocking/BookingHelperTests.cs
+++ TestNinja.UnitTest/Mocking/BookingHelperTests.cs
@@ -84,14 +84,46 @@ namespace TestNinja.UnitTest.Mocking
         }
 
         [Test]
-        public void CancelledBooking_ReturnEmptyString()
+        public void BookingStartsInTheMiddleAndFinishesAfterExistingBooking_ReturnExistingBookingReference()
         {
             var result = BookingHelper.OverlappingBookingsExist(
                 new Booking
                 {
                     Id = 1,
-                    ArrivalDate = new DateTime(2020, 10, 10, 14, 0, 0),
-                    DepartureDate = new DateTime(2020, 10, 14, 10, 0, 0),
+                    ArrivalDate = After(this.existingBooking.ArrivalDate),
+                    DepartureDate = After(this.existingBooking.DepartureDate, days: 2),
+                    Reference = "a"
+                },
+                this.bookingRepository.Object);
+
+            Assert.That(result, Is.EqualTo(this.existingBooking.Reference));
+        }
+
+        [Test]
+        public void BookingStartsAndFinishesAfterExistingBooking_ReturnEmptyString()
+        {
+            var result = BookingHelper.OverlappingBookingsExist(
+                new Booking
+                {
+                    Id = 1,
+                    ArrivalDate = After(this.existingBooking.DepartureDate),
+                    DepartureDate = After(this.existingBooking.DepartureDate, days: 2),
+                    Reference = "a"
+                },
+                this.bookingRepository.Object);
+
+            Assert.That(result, Is.Empty);
+        }
+
+        [Test]
+        public void OverlapingBookingButNewBookingIsCancelled_ReturnEmptyString()
+        {
+            var result = BookingHelper.OverlappingBookingsExist(
+                new Booking
+                {
+                    Id = 1,
+                    ArrivalDate = Before(this.existingBooking.ArrivalDate),
+                    DepartureDate = After(this.existingBooking.ArrivalDate),
                     Reference = "a",
                     Status = "Cancelled"
                 },
