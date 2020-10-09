17d2035274d09b1ef672ba17a6f2510f64d8898d
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-10-09 15:35:03 +0300

    Refactor Booking helper unit tests

--- TestNinja.UnitTest/Mocking/BookingHelperTests.cs
+++ TestNinja.UnitTest/Mocking/BookingHelperTests.cs
@@ -5,8 +5,6 @@ using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using TestNinja.Mocking;
 
@@ -16,35 +14,36 @@ namespace TestNinja.UnitTest.Mocking
     public class BookingHelper_OverlappingBookingsExistTests
     {
         private Mock<IBookingRepository> bookingRepository;
+        private Booking existingBooking;
 
         [SetUp]
         public void SetUp()
         {
             this.bookingRepository = new Mock<IBookingRepository>();
+            this.existingBooking = new Booking
+            {
+                Id = 2,
+                ArrivalDate = ArriveOn(2020, 10, 15),
+                DepartureDate = DepartOn(2020, 10, 20),
+                Reference = "a"
+            };
+            this.bookingRepository.Setup(x => x.GetActiveBookings(1)).Returns(
+                new List<Booking>
+                {
+                    this.existingBooking
+                }.AsQueryable()
+                );
         }
 
         [Test]
         public void BookingStartsAndFinishesBeforeAnExistingBooking_ReturnEmptyString()
         {
-            this.bookingRepository.Setup(x => x.GetActiveBookings(1)).Returns(
-                new List<Booking>
-                {
-                    new Booking
-                    {
-                        Id = 2,
-                        ArrivalDate = new DateTime(2020, 10, 15, 14, 0, 0),
-                        DepartureDate = new DateTime(2020, 10, 20, 10, 0, 0),
-                        Reference = "a"
-                    }
-                }.AsQueryable()
-                );
-
             var result = BookingHelper.OverlappingBookingsExist(
                 new Booking
                 {
                     Id = 1,
-                    ArrivalDate = new DateTime(2020, 10, 10, 14, 0, 0),
-                    DepartureDate = new DateTime(2020, 10, 14, 10, 0, 0),
+                    ArrivalDate = Before(this.existingBooking.ArrivalDate, days: 2),
+                    DepartureDate = Before(this.existingBooking.ArrivalDate),
                     Reference = "a"
                 },
                 this.bookingRepository.Object);
@@ -67,6 +66,26 @@ namespace TestNinja.UnitTest.Mocking
                 this.bookingRepository.Object);
 
             Assert.That(result, Is.Empty);
+        }
+
+        private DateTime ArriveOn(int year, int month, int day)
+        {
+            return new DateTime(year, month, day, 14, 0, 0);
+        }
+
+        private DateTime DepartOn(int year, int month, int day)
+        {
+            return new DateTime(year, month, day, 10, 0, 0);
+        }
+
+        private DateTime Before(DateTime date, int days = 1) 
+        {
+            return date.AddDays(-days);
+        }
+
+        private DateTime After(DateTime date, int days = 1)
+        {
+            return date.AddDays(days);
         }
     }
 }
